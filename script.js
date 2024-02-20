2a4296258b953dc499105add94fcf91b4601bcf5
Carlos Eduardo Mizerkowski <mizerkowski.carlos@escola.pr.gov.br>
2024-02-20 10:26:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let idade =19;
+ 
+if(idade>18){
+    console.log('${idede}anos-> maior de idade')
+} else if (idade == 18){
+    console.log('${idade}anos-> está no limete!')
+} else{
+    console.log('${idade}anos-> menor de idade')
+    
+}
+    
